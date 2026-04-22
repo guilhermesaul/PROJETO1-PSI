3111fe7019938c3cc51863f3f0d92c8425f8974f
Guilherme Saul <guilhermesaul190@gmail.com>
2026-04-22 14:20:35 -0300

    header e footer feito (com css)

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def cadastro():
+def login():
     return render_template('login.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
 
 @app.route('/home', methods = ['post'])
 def home():
